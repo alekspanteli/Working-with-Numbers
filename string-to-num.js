2c140c3bd5e1e3dd897dfb7beb93f092a21f54df
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-12 16:13:14 +0300

    1 file modified

--- string-to-num.js
+++ string-to-num.js
@@ -1,5 +1,14 @@
+// const HTMLBadges = prompt('Tets HTML ');
+// const CSSBadges = prompt('Tets CSS');
+
+// const totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges);
+
+// const pi = "3.14"
+// console.log(typeof parseFloat(pi));
+
+
 const HTMLBadges = prompt('Tets HTML ');
 const CSSBadges = prompt('Tets CSS');
 
-const totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges);
+const totalBadges = +HTMLBadges + +CSSBadges;
 console.log(totalBadges);
